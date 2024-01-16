c00649b0a5f474814ef3c8dee279bb7d60efc80a
Mario Cosmin Botez <botezmariocosmin@gmail.com>
2024-01-16 22:20:44 +0100

    visualizzazione tabelle db

--- DigitalGameStoreDBViewer/Form1.Designer.cs
+++ DigitalGameStoreDBViewer/Form1.Designer.cs
@@ -30,26 +30,26 @@
         {
             this.tabControl = new System.Windows.Forms.TabControl();
             this.tabPageUtenti = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.tabPageProdotti = new System.Windows.Forms.TabPage();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.tabPageAcquisti = new System.Windows.Forms.TabPage();
-            this.tabPageRecensioni = new System.Windows.Forms.TabPage();
-            this.tabPagePromozioni = new System.Windows.Forms.TabPage();
             this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.tabPageRecensioni = new System.Windows.Forms.TabPage();
             this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.tabPagePromozioni = new System.Windows.Forms.TabPage();
             this.dataGridView5 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.tabControl.SuspendLayout();
             this.tabPageUtenti.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabPageProdotti.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabPageAcquisti.SuspendLayout();
-            this.tabPageRecensioni.SuspendLayout();
-            this.tabPagePromozioni.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.tabPageRecensioni.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            this.tabPagePromozioni.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
             this.SuspendLayout();
             // 
@@ -60,31 +60,44 @@
             this.tabControl.Controls.Add(this.tabPageAcquisti);
             this.tabControl.Controls.Add(this.tabPageRecensioni);
             this.tabControl.Controls.Add(this.tabPagePromozioni);
-            this.tabControl.Location = new System.Drawing.Point(12, 12);
+            this.tabControl.Location = new System.Drawing.Point(18, 18);
+            this.tabControl.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(776, 426);
+            this.tabControl.Size = new System.Drawing.Size(1164, 655);
             this.tabControl.TabIndex = 0;
             // 
             // tabPageUtenti
             // 
-            this.tabPageUtenti.Controls.Add(this.button1);
+            this.tabPageUtenti.Controls.Add(this.textBox1);
             this.tabPageUtenti.Controls.Add(this.dataGridView1);
-            this.tabPageUtenti.Location = new System.Drawing.Point(4, 22);
+            this.tabPageUtenti.Location = new System.Drawing.Point(4, 29);
+            this.tabPageUtenti.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabPageUtenti.Name = "tabPageUtenti";
-            this.tabPageUtenti.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageUtenti.Size = new System.Drawing.Size(768, 400);
+            this.tabPageUtenti.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPageUtenti.Size = new System.Drawing.Size(1156, 622);
             this.tabPageUtenti.TabIndex = 0;
             this.tabPageUtenti.Text = "Utenti";
             this.tabPageUtenti.UseVisualStyleBackColor = true;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(506, 9);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(638, 582);
+            this.dataGridView1.TabIndex = 2;
+            // 
             // tabPageProdotti
             // 
             this.tabPageProdotti.Controls.Add(this.dataGridView2);
-            this.tabPageProdotti.Location = new System.Drawing.Point(4, 22);
+            this.tabPageProdotti.Location = new System.Drawing.Point(4, 29);
+            this.tabPageProdotti.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabPageProdotti.Name = "tabPageProdotti";
-            this.tabPageProdotti.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageProdotti.Size = new System.Drawing.Size(768, 400);
+            this.tabPageProdotti.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPageProdotti.Size = new System.Drawing.Size(1156, 622);
             this.tabPageProdotti.TabIndex = 1;
             this.tabPageProdotti.Text = "Prodotti";
             this.tabPageProdotti.UseVisualStyleBackColor = true;
@@ -92,104 +105,107 @@
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(337, 6);
+            this.dataGridView2.Location = new System.Drawing.Point(506, 9);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(425, 378);
+            this.dataGridView2.RowHeadersWidth = 62;
+            this.dataGridView2.Size = new System.Drawing.Size(638, 582);
             this.dataGridView2.TabIndex = 1;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(337, 6);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(425, 378);
-            this.dataGridView1.TabIndex = 2;
             // 
             // tabPageAcquisti
             // 
             this.tabPageAcquisti.Controls.Add(this.dataGridView3);
-            this.tabPageAcquisti.Location = new System.Drawing.Point(4, 22);
+            this.tabPageAcquisti.Location = new System.Drawing.Point(4, 29);
+            this.tabPageAcquisti.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabPageAcquisti.Name = "tabPageAcquisti";
-            this.tabPageAcquisti.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageAcquisti.Size = new System.Drawing.Size(768, 400);
+            this.tabPageAcquisti.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPageAcquisti.Size = new System.Drawing.Size(1156, 622);
             this.tabPageAcquisti.TabIndex = 2;
             this.tabPageAcquisti.Text = "Acquisti";
             this.tabPageAcquisti.UseVisualStyleBackColor = true;
             // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(506, 9);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 62;
+            this.dataGridView3.Size = new System.Drawing.Size(638, 582);
+            this.dataGridView3.TabIndex = 2;
+            // 
             // tabPageRecensioni
             // 
             this.tabPageRecensioni.Controls.Add(this.dataGridView4);
-            this.tabPageRecensioni.Location = new System.Drawing.Point(4, 22);
+            this.tabPageRecensioni.Location = new System.Drawing.Point(4, 29);
+            this.tabPageRecensioni.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabPageRecensioni.Name = "tabPageRecensioni";
-            this.tabPageRecensioni.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageRecensioni.Size = new System.Drawing.Size(768, 400);
+            this.tabPageRecensioni.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPageRecensioni.Size = new System.Drawing.Size(1156, 622);
             this.tabPageRecensioni.TabIndex = 3;
             this.tabPageRecensioni.Text = "Recensioni";
             this.tabPageRecensioni.UseVisualStyleBackColor = true;
             // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(506, 9);
+            this.dataGridView4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.RowHeadersWidth = 62;
+            this.dataGridView4.Size = new System.Drawing.Size(638, 582);
+            this.dataGridView4.TabIndex = 2;
+            // 
             // tabPagePromozioni
             // 
             this.tabPagePromozioni.Controls.Add(this.dataGridView5);
-            this.tabPagePromozioni.Location = new System.Drawing.Point(4, 22);
+            this.tabPagePromozioni.Location = new System.Drawing.Point(4, 29);
+            this.tabPagePromozioni.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabPagePromozioni.Name = "tabPagePromozioni";
-            this.tabPagePromozioni.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPagePromozioni.Size = new System.Drawing.Size(768, 400);
+            this.tabPagePromozioni.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPagePromozioni.Size = new System.Drawing.Size(1156, 622);
             this.tabPagePromozioni.TabIndex = 4;
             this.tabPagePromozioni.Text = "Promozioni";
             this.tabPagePromozioni.UseVisualStyleBackColor = true;
             // 
-            // dataGridView3
-            // 
-            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(337, 6);
-            this.dataGridView3.Name = "dataGridView3";
-            this.dataGridView3.Size = new System.Drawing.Size(425, 378);
-            this.dataGridView3.TabIndex = 2;
-            // 
-            // dataGridView4
-            // 
-            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView4.Location = new System.Drawing.Point(337, 6);
-            this.dataGridView4.Name = "dataGridView4";
-            this.dataGridView4.Size = new System.Drawing.Size(425, 378);
-            this.dataGridView4.TabIndex = 2;
-            // 
             // dataGridView5
             // 
             this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView5.Location = new System.Drawing.Point(337, 6);
+            this.dataGridView5.Location = new System.Drawing.Point(506, 9);
+            this.dataGridView5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView5.Name = "dataGridView5";
-            this.dataGridView5.Size = new System.Drawing.Size(425, 378);
+            this.dataGridView5.RowHeadersWidth = 62;
+            this.dataGridView5.Size = new System.Drawing.Size(638, 582);
             this.dataGridView5.TabIndex = 2;
             // 
-            // button1
+            // textBox1
             // 
-            this.button1.Location = new System.Drawing.Point(110, 185);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.textBox1.Location = new System.Drawing.Point(45, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(286, 26);
+            this.textBox1.TabIndex = 4;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.tabControl);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "DigitalGameStoreDBViewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl.ResumeLayout(false);
             this.tabPageUtenti.ResumeLayout(false);
+            this.tabPageUtenti.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.tabPageProdotti.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.tabPageAcquisti.ResumeLayout(false);
-            this.tabPageRecensioni.ResumeLayout(false);
-            this.tabPagePromozioni.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.tabPageRecensioni.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            this.tabPagePromozioni.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
             this.ResumeLayout(false);
 
@@ -208,7 +224,7 @@
         private System.Windows.Forms.DataGridView dataGridView4;
         private System.Windows.Forms.TabPage tabPagePromozioni;
         private System.Windows.Forms.DataGridView dataGridView5;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- DigitalGameStoreDBViewer/Form1.cs
+++ DigitalGameStoreDBViewer/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace DigitalGameStoreDBViewer
 {
@@ -16,10 +18,106 @@ namespace DigitalGameStoreDBViewer
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void LoadDataIntoGridView()
         {
+            try
+            {
+                String ConnectionString = "server=127.0.0.1;uid=programma;pwd=12345;database=digitalgamestore";
+                MySqlConnection conn = new MySqlConnection(ConnectionString);
 
+                conn.Open();
+
+                String sql1 = "select * from utenti;";
+
+                MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
+                cmd1.ExecuteNonQuery();
+
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = cmd1;
+                DataTable dati = new DataTable();
+                MyAdapter.Fill(dati);
+                dataGridView1.DataSource = dati;
+                conn.Close();
+
+                //////
+
+                conn.Open();
+
+                sql1 = "select * from prodotti;";
+
+                cmd1 = new MySqlCommand(sql1, conn);
+                cmd1.ExecuteNonQuery();
+
+                MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = cmd1;
+                dati = new DataTable();
+                MyAdapter.Fill(dati);
+                dataGridView2.DataSource = dati;
+                conn.Close();
+
+                //////
+
+                conn.Open();
+
+                sql1 = "select * from acquisti;";
+
+                cmd1 = new MySqlCommand(sql1, conn);
+                cmd1.ExecuteNonQuery();
+
+                MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = cmd1;
+                dati = new DataTable();
+                MyAdapter.Fill(dati);
+                dataGridView3.DataSource = dati;
+                conn.Close();
+
+                //////
+               
+                conn.Open();
+
+                sql1 = "select * from recensioni;";
+
+                cmd1 = new MySqlCommand(sql1, conn);
+                cmd1.ExecuteNonQuery();
+
+                MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = cmd1;
+                dati = new DataTable();
+                MyAdapter.Fill(dati);
+                dataGridView4.DataSource = dati;
+                conn.Close();
+
+                //////
+
+                conn.Open();
+
+                sql1 = "select * from promozioni;";
+
+                cmd1 = new MySqlCommand(sql1, conn);
+                cmd1.ExecuteNonQuery();
+
+                MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = cmd1;
+                dati = new DataTable();
+                MyAdapter.Fill(dati);
+                dataGridView5.DataSource = dati;
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore: " + ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView3.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView4.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView5.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            tabControl.SelectedIndex = 0;
+            LoadDataIntoGridView();
         }
     }
 }
